8afe724a755f3cc561f316080d3963390e0cb434
Tanish Pandey <tanishpandey14@gmail.com>
2024-04-03 21:27:11 +0530

    js interview questions

---
+++ Question 16/script.js
@@ -0,0 +1,6 @@
+function setCookie()  
+{  
+    document.cookie="username=Duke Martin";  
+}   
+
+setCookie()
---
+++ Question 17/script.js
@@ -0,0 +1,4 @@
+ document.cookie="username=Duke Martin";  
+let x = document.cookie;
+
+document.write(x)
