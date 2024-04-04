004755c8262837e1e2c653d2e28768873dafc8eb
Tanish Pandey <tanishpandey14@gmail.com>
2024-04-04 11:05:37 +0530

    interview question practice

---
+++ Question 11/script.js
@@ -0,0 +1,4 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let result = Array.isArray(fruits);
+
+console.log(result)
---
+++ Question 9/script.js
@@ -0,0 +1,6 @@
+
+function greet() {
+    console.log('Hello world');
+}
+
+setInterval(greet, 1000);
