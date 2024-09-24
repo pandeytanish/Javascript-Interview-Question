0aff1e26bf461a15793bade056621da82746e3f5
Tanish Pandey <tanishpandey14@gmail.com>
2024-09-24 23:56:12 +0530

    js practice

--- Question 5/Script.js
+++ Question 5/Script.js
@@ -1,3 +1,7 @@
 var head2 = document.getElementById('heading');
 head2.innerHTML = '<h1>hello world</h1>';
 console.log(head2);
+
+var head2 = document.getElementById('heading');
+head2.innerHTML = '<h1>hello world</h1>';
+console.log(head2);
