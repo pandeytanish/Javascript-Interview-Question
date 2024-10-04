e6d4ee2a83a52883057988c96ae92f4b76230dd3
Tanish Pandey <tanishpandey14@gmail.com>
2024-10-04 22:29:44 +0530

    javascript

--- Question 2/script.js
+++ Question 2/script.js
@@ -2,3 +2,12 @@
 let x = document.cookie;
 
 document.write(x)
+
+document.cookie="username=Duke Martin";  
+let x = document.cookie;
+
+document.write(x)
+document.cookie="username=Duke Martin";  
+let x = document.cookie;
+
+document.write(x)
